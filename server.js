84f4ee6fc2a8d3e136cf0d88540130304db4f598
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2022-06-01 17:42:23 -0500

    sudoku-solver

---
+++ server.js
@@ -0,0 +1,36 @@
+const PORT = 8000
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post('/solve', (req, res) => {
+  const options = {
+    method: 'POST',
+    url: 'https://solve-sudoku.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+    },
+    data: {
+      puzzle: req.body.numbers
+    },
+  }
+
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data)
+      res.json(response.data)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+})
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`))
